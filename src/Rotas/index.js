4b5c2855650e7ddfdd95dab7c0fa8b1031fdb273
Marcos André Ribeiro Gomes <marcosandrebahia@gmail.com>
2021-06-04 00:41:41 -0300

    CheckIn e CheckOut
    Inicio das páginas de checkIn e CheckOut

--- src/Rotas/index.js
+++ src/Rotas/index.js
@@ -4,14 +4,17 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from '../paginas/Home';
 import Contato from '../paginas/Contato';
 import Login from '../paginas/Login';
-
+import CheckIn from '../paginas/CheckIn';
+import CheckOut from '../paginas/CheckOut';
 
 const Rotas = () => (
     <BrowserRouter>
         <Switch>
             <Route exact path='/' component={ Home } />
-            <Route exact path='/contato' component={ Contato } />
-            <Route exact path='/Login' component={ Login } />
+            <Route path='/contato' component={ Contato } />
+            <Route path='/Login' component={ Login } />
+            <Route path='/CheckIn' component ={CheckIn}/>
+            <Route path='/CheckOut' component ={CheckOut}/>
         </Switch>
     </BrowserRouter>
 );
---
+++ src/paginas/CheckIn/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Topo from '../../componentes/Topo';
+import Rodape from '../../componentes/Rodape';
+
+const CheckIn = () => {
+    return(
+        <div>
+            <Topo/>
+            <Rodape/>
+        </div>
+
+    );
+}
+
+export default CheckIn;
